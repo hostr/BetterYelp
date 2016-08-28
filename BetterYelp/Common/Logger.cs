0e380c856f588f768b863b4f62b035a2b7d97290
rhost <ryanhostrh@gmail.com>
2016-08-28 15:05:08 -0500

    Swap ELMAH for log4net add logging class

---
+++ BetterYelp/Common/Logger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Elmah;
+
+namespace BetterYelp.Common
+{
+    public static class Logger
+    {
+        public static void LogError
+    }
+}
